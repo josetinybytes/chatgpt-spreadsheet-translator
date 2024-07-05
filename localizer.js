d34c5b6e9f6b5850d3177c6ebba52567c05f10ae
Jose Guerra <josguerra99@gmail.com>
2024-07-04 18:13:55 -0600

    open ai rate limit and token amounts optimization

--- localizer.js
+++ localizer.js
@@ -4,13 +4,14 @@ const { SheetsCache } = require('./sheetsHelper');
 const { Semaphore, withRetry } = require("./utils");
 
 const PARALLEL_TASKS = parseInt(process.env.PARALLEL_TASKS) || 5;
+const BATCHED_LANGUAGES_SIZE = parseInt(process.env.BATCHED_LANGUAGES_SIZE) || 5;
+const MAX_TEXT_LENGTH_FOR_BATCHING_LANGUAGES = parseInt(process.env.MAX_TEXT_LENGTH_FOR_BATCHING_LANGUAGES) || 300;
 
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
   organization: process.env.OPENAI_API_ORG,
 });
-
 
 
 
@@ -35,56 +36,95 @@ async function localizeTexts(
 
   if (gptVersion == null)
     gptVersion = "gpt-4o";
-  let response = await openai.chat.completions.create({
-    model: gptVersion,
-    response_format: { "type": "json_object" },
-    messages: [
-      {
-        role: "system",
-        content:
-          `You are a helpful assistant tasked with translating English keys into specified language codes for a video game. 
+
+
+  let featuresToKeep = {};
+  //Let's remove the non used features keys from the game context
+  //Lets iterate over the features
+  for (let featureKey in gameContext.features) {
+    //Now remove all the keys from  gameContext.features[featureKey] that are not in the languageCodesToTranslateTo array and is not context
+    for (let languageCode in gameContext.features[featureKey]) {
+      if (!languageCodesToTranslateTo.includes(languageCode) && languageCode != 'context' && languageCode != 'en') {
+        delete gameContext.features[featureKey][languageCode];
+      }
+    }
+    if (englishText.toLowerCase().trim().includes(gameContext.features[featureKey][`en`].toLowerCase().trim()))
+      featuresToKeep[featureKey] = gameContext.features[featureKey];
+
+  }
+
+
+  let response = null;
+  let toTranslate = JSON.stringify({
+    key: keyName,
+    en: englishText,
+    context: textContext,
+    languagesToRetrieve: languageCodesToTranslateTo,
+    featureNames: featuresToKeep,
+  });
+  try {
+    response = await openai.chat.completions.create({
+      model: gptVersion,
+      response_format: { "type": "json_object" },
+      messages: [
+        {
+          role: "system",
+          content:
+            `You are a helpful assistant tasked with translating English keys into specified language codes for a video game. 
                 Your translations should be accurate and context-appropriate.`
-      },
-      {
-        role: "system",
-        content: gameContext.description
-      },
-      {
-        role: "system",
-        content:
-          `Respond with a JSON object in the format {"languageCode": {"key":"translatedValue", ...}}. Ensure the JSON object is minified.`
-      },
-      {
-        role: "system",
-        content:
-          `If unable to translate a key, include it in a "missing" section like this: "missing":[{"key":"originalKey", "reason":"explanation"}]. 
+        },
+        {
+          role: "system",
+          content: gameContext.description
+        },
+        {
+          role: "system",
+          content:
+            `Respond with a JSON object in the format {"languageCode": {"key":"translatedValue", ...}}. 
+          Ensure the JSON object is minified. Where key is the original localizationKey and translatedValue is the translated value.
+          For example: when provided {"key":"hello","en":"Hello","languagesToRetrieve":["es"]} the response should be {"es":{"hello":"Hola"}}`
+        },
+        {
+          role: "system",
+          content:
+            `If unable to translate a key, include it in a "missing" section like this: "missing":[{"key":"originalKey", "reason":"explanation"}]. 
                 Provide a reason for each untranslated key.`
-      },
-      {
-        role: "system",
-        content:
-          `Expect translation requests in JSON format: {"key":"localizationKey", "en":"englishText" ,"context":"contextForTheText", "languagesToRetrieve":["languageCode1", ...], "featureNames": {"featureKey":{"languageCode":"Feature Translation", ...,"context":"Context for the feature"}}}. 
+        },
+        {
+          role: "system",
+          content:
+            `Expect translation requests in JSON format: {"key":"localizationKey", "en":"englishText" ,"context":"contextForTheText", "languagesToRetrieve":["languageCode1", ...], "featureNames": {"featureKey":{"languageCode":"Feature Translation", ...,"context":"Context for the feature"}}}. 
                 Translate the English text into the requested languages. 
                 Use the provided context for the text, feature names and features context to provide accurate translations.
                 The text might contain HTML or rich text formatting, so please ensure that the translation is accurate and preserves the formatting.
-                Also it can contain string formatters like {0}, {1}, etc. Please ensure that the translation preserves the string formatters.`
+                Also it can contain string formatters like {0}, {1}, etc. Please ensure that the translation preserves the string formatters.
+                Formatters also can be {%0}, {%1}, etc. Please ensure that the translation preserves the formatters event for RTL languages don't mix {0} and {%0}`
 
-      },
-      {
-        role: "user",
-        content: JSON.stringify({
-          key: keyName,
-          en: englishText,
-          context: textContext,
-          languagesToRetrieve: languageCodesToTranslateTo,
-          featureNames: gameContext.features,
-        }),
-      },
-    ],
+        },
+        {
+          role: "user",
+          content: toTranslate
+        },
+      ],
 
-  });
+    });
+  } catch (e) {
+    if (e.status == 429) {
+      console.log(`Too many requests. Waiting for ${e.headers['retry-after']} seconds before retrying`);
+      await sleep(e.headers['retry-after-ms']);
+      return await localizeTexts(keyName, englishText, languageCodesToTranslateTo, gameContext, textContext, gptVersion);
+    }
 
-  return JSON.parse(response.choices[0].message.content);
+    throw e;
+  }
+
+  try {
+
+    return JSON.parse(response.choices[0].message.content);
+  } catch (e) {
+    console.log(response.choices[0]);
+    throw e;
+  }
 }
 
 
@@ -192,13 +232,46 @@ async function translateSpreadsheet(sheetsCache, sheetId, gameContext, gptVersio
 
 
     if (missingKeys.length > 0) {
-      toTranslate.push({
-        key: key,
-        en: en,
-        languageCodesToTranslateTo: missingKeys,
-        context: currentContext,
-        rowIndex: i
-      })
+
+      if (en.length <= MAX_TEXT_LENGTH_FOR_BATCHING_LANGUAGES) {
+        //If not we from the missing translation , we will do them in batches, we will ask the API to translate the text in multiple languages at once
+        //we need to separate the missingKeys array into multiple array of MAX_BATCHED_LANGUAGES_SIZE 
+        let missingKeysBatches = [];
+        for (let i = 0; i < missingKeys.length; i += BATCHED_LANGUAGES_SIZE) {
+          missingKeysBatches.push(missingKeys.slice(i, i + BATCHED_LANGUAGES_SIZE));
+        }
+
+
+        missingKeysBatches.forEach((languaceCodes) => {
+          toTranslate.push({
+            key: key,
+            en: en,
+            languageCodesToTranslateTo: languaceCodes,
+            context: currentContext,
+            rowIndex: i
+          });
+        })
+
+
+      } else {
+
+
+        //If the english text is too long, we split the translation into multiple requests, instead of one request with all the languages , we will do one request per language
+        //This is to avoid the OpenAI API returning an error saying that the text is too long
+
+        missingKeys.forEach((languaceCode) => {
+          toTranslate.push({
+            key: key,
+            en: en,
+            languageCodesToTranslateTo: [languaceCode],
+            context: currentContext,
+            rowIndex: i
+          });
+        })
+
+
+
+      }
     }
   }
 
@@ -213,15 +286,17 @@ async function translateSpreadsheet(sheetsCache, sheetId, gameContext, gptVersio
       t = async (index) => {
         let translateItem = toTranslate[i];
 
-        console.log(`Translating (${index}) [${translateItem.key}]`);
-        console.time(`Translating (${index}) [${translateItem.key}]`);
+        console.log(`Translating (${index}) [${translateItem.key}] (${translateItem.languageCodesToTranslateTo.join(', ')})`);
+        console.time(`Translating (${index}) [${translateItem.key}] (${translateItem.languageCodesToTranslateTo.join(', ')})`);
         await sleep(100 * index);//Offset the time for each request, so we don't get rate limited 
         let result = await withRetry(async () => await localizeTexts(translateItem.key, translateItem.en, translateItem.languageCodesToTranslateTo, gameContext, translateItem.context, gptVersion), 3, 3000);
-        console.timeEnd(`Translating (${index}) [${translateItem.key}]`);
+        console.timeEnd(`Translating (${index}) [${translateItem.key}] (${translateItem.languageCodesToTranslateTo.join(', ')})`);
 
 
+        //console.log(result);
         for (let j = 0; j < header.length; j++) {
           let languageCode = getLanguageCode(header[j]);
+
           if (languageCode == '' || result[languageCode] == null)
             continue;
 
